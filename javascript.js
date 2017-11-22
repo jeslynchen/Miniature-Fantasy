e83041cad7c3c1f53b4db02e90e1e67fe56b8ac0
jeslynchen <33896463+jeslynchen@users.noreply.github.com>
2017-11-22 22:17:15 +0800

    upload the html files

---
+++ javascript.js
@@ -0,0 +1,12 @@
+/* 
+    IBM In-house {Web Coder} course Assignment Snippet.
+    JavaScript file.
+    For internal IBM use.
+*/
+
+// JavaSCript goes here...
+
+// function submitData(){
+//     var form =document.getElementId('myFrom');
+//     google.script.run.withFailureHandler(alert).withSuccessHandler(alret)
+// }
